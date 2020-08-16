e503f458fe94bc4e339df5008859d606583a7a4a
ZGingko <zhengcanping147@sina.com>
2020-08-16 23:51:51 +0800

    fixed

--- week04/pandas_test.py
+++ week04/pandas_test.py
@@ -1,36 +1,43 @@
 import pymysql
 import pandas as pd
 
-db = pymysql.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    password="Aa123456",
-    database="testdb",
-    charset="utf8mb4"
-)
+dbInfo = {
+    'host': '192.168.1.103',
+    'port': 3306,
+    'user': 'root',
+    'password': 'root123',
+    'db': 'learn_python'
+}
 
-data = pd.read_sql("select * from users", db)
+conn = pymysql.connect(
+            host=dbInfo['host'],
+            port=dbInfo['port'],
+            user=dbInfo['user'],
+            password=dbInfo['password'],
+            db=dbInfo['db']
+        )
+
+data = pd.read_sql("select * from t_pandas_test", conn)
 
 # 1. SELECT * FROM data;
 print(data)
 
 # 2. SELECT * FROM data LIMIT 10;
-data[:10]
+print(data[:10])
 
 # 3. SELECT id FROM data; //id 是 data 表的特定一列
-data["id"]
+print(data["id"])
 
 # 4. SELECT COUNT(id) FROM data;
-data["id"].count()
+print(data["id"].count())
 
 # 5. SELECT * FROM data WHERE id<1000 AND age>30;
-data[(data["id"] < 1000) & (data["age"] > 30)]
+print(data[(data["id"] < 1000) & (data["age"] > 30)])
 
 # 6. SELECT id,COUNT(DISTINCT order_id) FROM table1 GROUP BY id;
 # unique()是以 数组形式（numpy.ndarray）返回列的所有唯一值（特征的所有唯一值）
 # nunique() Return number of unique elements in the object.即返回的是唯一值的个数
-data.groupby("id").agg({"order_id": "nunique"})
+print(data.groupby("id").agg({"order_id": "nunique"}))
 
 # 7. SELECT * FROM table1 t1 INNER JOIN table2 t2 ON t1.id = t2.id;
 data1 = pd.read_sql("select * from table1", db)
