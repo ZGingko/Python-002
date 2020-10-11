dbbf68c620c31ae606c9e0571f2b40f0b078f368
ZGingko <zhengcanping147@sina.com>
2020-10-11 22:31:47 +0800

    fixed homework week10&11

--- week10/homework/index/urls.py
+++ week10/homework/index/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.mylogin),
+    path('login', views.mylogin)
 ]
--- week10/homework/index/views.py
+++ week10/homework/index/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .form import LoginForm
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
+
 def index(request):
     return HttpResponse("Hello Django!")
+
+
+def mylogin(request):
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            cd = login_form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user:
+                login(request, user)
+                return render(request, 'index.html', {'form': login_form}) # HttpResponse('登录成功')
+            else:
+                return HttpResponse('登录失败')
+    
+    if request.method == 'GET':
+        login_form = LoginForm()
+        return render(request, 'login.html', {'form': login_form})
